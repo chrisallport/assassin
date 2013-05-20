4e620fb4f3badb8d78d6ba1de41920a98dc23f61
Chris Allport <allportdc@yahoo.com>
2013-05-19 21:27:53 -0400

    added check if user is an admin for specific game

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,23 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :players
+
+  def is_admin_for_game game
+    return true if admin
+    player = player_for_game game.id
+    return player.game_admin if player 
+    return false 
+  end
+
+  private
+
+  def player_for_game game_id
+    players.where(game_id: game_id).first
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is an admin for any game if the user is an admin" do
+    user = User.new
+    user.admin = true
+    game = Game.new
+    user.is_admin_for_game(game).should be true 
+  end
+
+  it "is an admin for a game if it's player for the game is an admin" do
+    user = User.new
+    game = Game.new
+    game.id = 1
+    player = Player.new
+    player.game = game
+    player.user = user
+    player.game_admin = true
+    user.stub(:player_for_game).and_return(player)
+    user.is_admin_for_game(game).should be true
+  end
+
+  it "is not an admin for a game if it is an admin for only other games"
 end
